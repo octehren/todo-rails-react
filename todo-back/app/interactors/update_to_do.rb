d13445c9083ed323f2c7a28e12e10dfd3ed14fac
Otavio <otamm@hotmail.com>
2019-06-26 09:17:01 +0000

    debugged status change check

--- todo-back/app/interactors/update_to_do.rb
+++ todo-back/app/interactors/update_to_do.rb
@@ -3,9 +3,10 @@ class UpdateToDo
 
     def call
         todo = ToDo.find(context.id)
+        status1 = todo.status
         if todo.update_attributes(context.todo_params)
             context.todo = todo
-            context.changed_status = todo.status_changed?
+            context.changed_status = status1 != todo.status
         else
             context.fail!(errors: todo.errors)
         end
