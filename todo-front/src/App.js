b80c7100570dafa360f332b9cb347901c4caed26
Otavio <otamm@hotmail.com>
2019-06-26 08:07:33 +0000

    removed unused files

--- todo-front/src/App.js
+++ todo-front/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import TodosContainer from './components/TodosContainer';
 
@@ -8,7 +7,6 @@ class App extends Component {
     return (
       <div className="container">
         <div className="app-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1>To-do List</h1>
         </div>
         <TodosContainer />
